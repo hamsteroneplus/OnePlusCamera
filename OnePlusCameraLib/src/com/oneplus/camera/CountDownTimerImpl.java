19da4836af10522674fa532c6f1e3d35d749ffa4
charles-lo <jerries.lo@gmail.com>
2015-05-12 18:06:10 +0800

    1. CountDownTimerImpl: adjust usage behavior according to the discussion

--- OnePlusCameraLib/src/com/oneplus/camera/CountDownTimerImpl.java
+++ OnePlusCameraLib/src/com/oneplus/camera/CountDownTimerImpl.java
@@ -5,6 +5,7 @@ import android.os.SystemClock;
 import com.oneplus.base.EventArgs;
 import com.oneplus.base.Handle;
 import com.oneplus.base.HandlerUtils;
+import com.oneplus.base.Log;
 
 public class CountDownTimerImpl extends CameraComponent implements CountDownTimer {
 	
@@ -14,18 +15,6 @@ public class CountDownTimerImpl extends CameraComponent implements CountDownTime
 	private long m_ElapsedTime = 0;
 	private Handle m_CurrentHandle = null;
 	// operations
-	private Handle m_CloseHandle = new Handle("CountDownTimer"){
-		// handle user cancel
-		@Override
-		protected void onClose(int flags) {
-			if(m_CurrentHandle != null && this == m_CurrentHandle){
-				raise(EVENT_CANCELLED,  EventArgs.EMPTY);
-				// stop timer
-				HandlerUtils.removeCallbacks(CountDownTimerImpl.this, m_Timer);
-				// reset compute data
-				resetComputeData();
-			}
-		}};;
 	private Runnable m_Timer = new Runnable() {
 		   @Override
 		   public void run() {
@@ -51,12 +40,28 @@ public class CountDownTimerImpl extends CameraComponent implements CountDownTime
 
 	@Override
 	public Handle start(long seconds, int flags) {
+		verifyAccess();
 		if(m_CurrentHandle != null){
+			Log.e(TAG, "Count Down running ");
 			return null;
 		}
-		verifyAccess();
+		if(seconds <= 0){
+			Log.e(TAG, "seconds must greater than zero ");
+			return null;
+		}
 		//init count down
-		m_CurrentHandle = m_CloseHandle;
+		m_CurrentHandle = new Handle("CountDownTimer"){
+			// handle user cancel
+			@Override
+			protected void onClose(int flags) {
+				if(this == m_CurrentHandle){
+					raise(EVENT_CANCELLED,  EventArgs.EMPTY);
+					// stop timer
+					HandlerUtils.removeCallbacks(CountDownTimerImpl.this, m_Timer);
+					// reset compute data
+					resetComputeData();
+				}
+			}};;
 		m_CountdownSecs = seconds;
 		setReadOnly(PROP_IS_STARTED, true);
 		setReadOnly(PROP_REMAINING_SECONDS, m_CountdownSecs);
