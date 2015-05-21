c00798a82770327235655ad503f5663fd2e9415b
charles-lo <jerries.lo@gmail.com>
2015-05-21 17:32:26 +0800

    1. adopt android default ThumbnailUtils crop tool and remove the similiar function code I wrote

--- OnePlusCameraLib/src/com/oneplus/camera/io/FileManagerImpl.java
+++ OnePlusCameraLib/src/com/oneplus/camera/io/FileManagerImpl.java
@@ -9,9 +9,7 @@ import java.util.List;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
 import android.graphics.Matrix;
-import android.graphics.RectF;
 import android.media.ThumbnailUtils;
 import android.os.Environment;
 import android.os.FileObserver;
@@ -119,10 +117,6 @@ final class FileManagerImpl extends CameraThreadComponent implements FileManager
 			return m_Path;
 		}
 
-		boolean getIsVertical() {
-			return m_IsVertical;
-		}
-
 		PhotoCallback getCallback() {
 			return m_callback;
 		}
@@ -140,54 +134,6 @@ final class FileManagerImpl extends CameraThreadComponent implements FileManager
 		bitmap = BitmapFactory.decodeFile(path, options);
 
 		return bitmap;
-	}
-
-	public Bitmap scaleCenterCrop(Bitmap source, int newWidth, int newHeight) {
-		if (source == null) {
-			Log.e(TAG, "error:: Bitmap is null");
-			return null;
-		}
-		if (newWidth <= 0) {
-			Log.e(TAG, "error:: newWidth less or equal 0");
-			return null;
-		}
-		if (newHeight <= 0) {
-			Log.e(TAG, "error:: newHeight less or equal 0");
-			return null;
-		}
-		int sourceWidth = source.getWidth();
-		int sourceHeight = source.getHeight();
-
-		// Compute the scaling factors to fit the new height and width,
-		// respectively.
-		// To cover the final image, the final scaling will be the bigger
-		// of these two.
-		float xScale = (float) newWidth / sourceWidth;
-		float yScale = (float) newHeight / sourceHeight;
-		float scale = Math.max(xScale, yScale);
-
-		// Now get the size of the source bitmap when scaled
-		float scaledWidth = scale * sourceWidth;
-		float scaledHeight = scale * sourceHeight;
-
-		// Let's find out the upper left coordinates if the scaled bitmap
-		// should be centered in the new size give by the parameters
-		float left = (newWidth - scaledWidth) / 2;
-		float top = (newHeight - scaledHeight) / 2;
-
-		// The target rectangle for the new, scaled version of the source
-		// bitmap will now
-		// be
-		RectF targetRect = new RectF(left, top, left + scaledWidth, top + scaledHeight);
-
-		// Finally, we create a new bitmap of the specified size and draw
-		// our new,
-		// scaled bitmap onto it.
-		Bitmap dest = Bitmap.createBitmap(newWidth, newHeight, source.getConfig());
-		Canvas canvas = new Canvas(dest);
-		canvas.drawBitmap(source, null, targetRect, null);
-
-		return dest;
 	}
 
 	public static int calculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight) {
@@ -337,7 +283,7 @@ final class FileManagerImpl extends CameraThreadComponent implements FileManager
 							bitmap = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
 						}
 
-						args.getCallback().onBitmapLoad(scaleCenterCrop(bitmap, msg.arg1, msg.arg2), isVideo);
+						args.getCallback().onBitmapLoad(ThumbnailUtils.extractThumbnail(bitmap, msg.arg1, msg.arg2), isVideo);
 						break;
 					}
 					}
